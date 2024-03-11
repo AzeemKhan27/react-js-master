b0637eb97280fbb5be330d1f66f6e0c16f98538c
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-03-11 12:43:59 +0530

    learnt complete react router build project

---
+++ 07reactRouter/src/components/index.js
@@ -0,0 +1,6 @@
+import Home from './Home/Home.jsx';
+import About from './About/About.jsx';
+import Footer from './Footer/Footer.jsx';
+import Header from './Header/Header.jsx';
+
+export {Home,About,Header,Footer}
