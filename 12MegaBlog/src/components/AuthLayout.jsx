44b7dafd2af4ff0329f4958116249aec7752e6bc
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-03-24 10:47:33 +0530

    add protected auth code

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react'
+import {useSelector} from "react-redux"
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+  return (
+    useEffect(()=>{
+        // TODO : 
+
+        //let authVal = authStatus === true ? true : false
+        if(authentication && authStatus !== authentication){   // true && false !== true ==> true && true ===> True
+            navigate('/login') 
+        }else if(!authentication && authStatus !== authentication){   // false && true !== true ===> false && false ===> True  
+            navigate('/')
+        }
+        setLoader(false)
+    },[authStatus, navigate])
+  )
+}
+
