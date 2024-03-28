ac8ee4c10c409722137b7e2a0cbc9d2dda57de23
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-03-29 00:10:55 +0530

    add logo

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import logoImage from "../../src/assets/logo1.jpg"
 
 function Logo({width = "100px"}) {
   return (
-    <div>Logo</div>
+    <img src={logoImage} style={{ width: '100px', height: 'auto', borderRadius: '50%' }}  alt="Logo" />
   )
 }
 
